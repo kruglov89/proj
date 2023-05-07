8712a82412501740e4368056fb8dff31ab3665f0
Sergey Kruglov <3120944@mail.ru>
2023-05-07 22:39:02 +0300

    добавлен вызов метода doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,6 +5,7 @@ public class MainApplication {
         System.out.println("---");
         System.out.println("Hello World!");
         System.out.println("10001");
+        doSomething();
         System.out.println("---");
     }
 
